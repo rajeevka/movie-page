88d226eefa57e61eab2e932828f1a8fc7ed75454
rajeevka <rke39412@gmail.com>
2023-02-23 17:19:35 +0530

    colorpicker

--- src/App.js
+++ src/App.js
@@ -1,65 +1,12 @@
-
+import Color from "./Color";
 import './App.css';
-import movie1 from './movie1.PNG'
- import movie1lgo from "./movie1logo1.PNG";
- import movie2logo2 from "./movie2logo2.PNG";
- import movie2 from "./movie2.PNG";
- import sidelogo from "./sidelogo.PNG";
+
 function App() {
   
   return (
     <div className="App">
-      <div className="maincontainer">
-        <div className="maincontainerone">
-          <div className="maincontaineroneleft">
-            <div className="maincontaineroneleft1">
-              <img src={movie1lgo} alt="img" />
-              <div className="maincontaineroneleftleft">
-                <h3>Bright</h3>
-                <b style={{ color: "blue" }}>2017,David Ayer</b>
-                <br />
-                <span style={{ border: "2px solid white" }}>117 min</span>
-                &nbsp;&nbsp;
-                <span>Action, Crime, Fantasy</span>
-              </div>
-            </div>
 
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea, vitae
-              consequatur iure ratione labore accusamus.
-            </p>
-            <img src={sidelogo} alt="img" />
-          </div>
-          <div className="maincontaineroneright">
-            <img src={movie1} alt="img" />
-          </div>
-        </div>
-        <div className="maincontainertwo">
-          <div className="maincontaineroneleft">
-            <div className="maincontaineroneleft1">
-              <img src={movie2logo2} alt="img" />
-              <div className="maincontaineroneleftleft">
-                <h4>Tomb Raider</h4>
-                <b style={{ color: "blue" }}>2018,Roar Uthaug</b>
-                <br />
-                <span style={{ border: "2px solid white" }}>125 min</span>
-                &nbsp;&nbsp;
-                <span>Action, Fantasy</span>
-              </div>
-            </div>
-
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea,
-              ectetur adipisicing elit. Ea, vitae consequatur iure ratione
-              labore accusamus.
-            </p>
-            <img src={sidelogo} alt="img" />
-          </div>
-          <div className="maincontaineroneright">
-            <img src={movie2} alt="img" />
-          </div>
-        </div>
-      </div>
+      <Color/>
     </div>
   );
 }
---
+++ src/Color.jsx
@@ -0,0 +1,112 @@
+import React from 'react'
+
+const Color = () => {
+  return (
+    <div>
+      <div className="maincontainer">
+        <div className="maincontainerone">
+          <div
+            className="maincontaineronecolor"
+            style={{ backgroundColor: "#FF6563" }}
+          ></div>
+          <div className="maincontaineronetext">
+            <h4>#FF6563</h4>
+            <span style={{ color: "#FF6563" }}>PINK</span>
+          </div>
+        </div>
+        <div className="maincontainerone">
+          <div
+            className="maincontaineronecolor"
+            style={{ backgroundColor: "#333333" }}
+          ></div>
+          <div className="maincontaineronetext">
+            <h4>#333333</h4>
+            <span style={{ color: "#333333" }}>GRAY</span>
+          </div>
+        </div>
+        <div className="maincontainerone">
+          <div
+            className="maincontaineronecolor"
+            style={{ backgroundColor: "#000000" }}
+          ></div>
+          <div className="maincontaineronetext">
+            <h4>#000000</h4>
+            <span style={{ color: "#000000" }}>BLACK</span>
+          </div>
+        </div>
+        <div className="maincontainerone">
+          <div
+            className="maincontaineronecolor"
+            style={{ backgroundColor: "#38BB14" }}
+          ></div>
+          <div className="maincontaineronetext">
+            <h4>#38BB14</h4>
+            <span style={{ color: "#38BB14" }}>GREEN</span>
+          </div>
+        </div>
+        <div className="maincontainerone">
+          <div
+            className="maincontaineronecolor"
+            style={{ backgroundColor: "#C90B0B" }}
+          ></div>
+          <div className="maincontaineronetext">
+            <h4>#C90B0B</h4>
+            <span style={{ color: "#C90B0B" }}>RED</span>
+          </div>
+        </div>
+        <div className="maincontainerone">
+          <div
+            className="maincontaineronecolor"
+            style={{ backgroundColor: "#FF8000" }}
+          ></div>
+          <div className="maincontaineronetext">
+            <h4>#FF8000</h4>
+            <span style={{ color: "#FF8000" }}>ORANGE</span>
+          </div>
+        </div>
+        <div className="maincontainerone">
+          <div
+            className="maincontaineronecolor"
+            style={{ backgroundColor: "#FFF500" }}
+          ></div>
+          <div className="maincontaineronetext">
+            <h4>#FFF500</h4>
+            <span style={{ color: "#FFF500" }}>YELLOW</span>
+          </div>
+        </div>
+        <div className="maincontainerone">
+          <div
+            className="maincontaineronecolor"
+            style={{ backgroundColor: "#CCCCCC" }}
+          ></div>
+          <div className="maincontaineronetext">
+            <h4>#CCCCCC</h4>
+            <span style={{ color: "#CCCCCC" }}>LIGHT GRAY</span>
+          </div>
+        </div>
+        <div className="maincontainerone">
+          <div
+            className="maincontaineronecolor"
+            style={{ backgroundColor: "#7E41A2" }}
+          ></div>
+          <div className="maincontaineronetext">
+            <h4>#7E41A2</h4>
+            <span style={{ color: "#7E41A2" }}>PURPLE</span>
+          </div>
+        </div>
+        <div className="maincontainerone">
+          <div
+            className="maincontaineronecolor"
+            style={{ backgroundColor: "#C14911" }}
+          ></div>
+          <div className="maincontaineronetext">
+            <h4>#C14911</h4>
+            <span style={{ color: "#C14911" }}>BROWN</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Color
